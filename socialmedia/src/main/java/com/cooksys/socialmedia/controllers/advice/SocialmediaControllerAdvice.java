62947ea039d0000fe981bfe14d2940c0b44426b1
PhamJimmy <JimmyPhamRes@gmail.com>
2023-11-28 09:51:25 -0800

    Error handling, added in not authorized exception

--- socialmedia/src/main/java/com/cooksys/socialmedia/controllers/advice/SocialmediaControllerAdvice.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/controllers/advice/SocialmediaControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.cooksys.socialmedia.controllers.advice;
 
 import com.cooksys.socialmedia.exceptions.BadRequestException;
+import com.cooksys.socialmedia.exceptions.NotAuthorizedException;
+import com.cooksys.socialmedia.exceptions.NotFoundException;
 import com.cooksys.socialmedia.model.ErrorDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,5 +17,17 @@ public class SocialmediaControllerAdvice {
   @ExceptionHandler(BadRequestException.class)
   public ErrorDto handleBadRequestException(BadRequestException badRequestException) {
     return new ErrorDto(badRequestException.getMessage());
+  }
+
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(NotFoundException.class)
+  public ErrorDto handleNotFoundException(NotFoundException notFoundException) {
+    return new ErrorDto(notFoundException.getMessage());
+  }
+
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
+  @ExceptionHandler(NotAuthorizedException.class)
+  public ErrorDto handleNotAuthorizedException(NotAuthorizedException notAuthorizedException) {
+    return new ErrorDto(notAuthorizedException.getMessage());
   }
 }
