5601bfddd9301b6f13c5545c466f967328ac4fb6
MKHLink <likhonhasankomol@gmail.com>
2023-11-28 13:51:14 -0500

    created followers_following join table using id and email from user_table

--- socialmedia/src/main/java/com/cooksys/socialmedia/entities/User.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/entities/User.java
@@ -1,14 +1,20 @@
 package com.cooksys.socialmedia.entities;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -40,4 +46,13 @@ public class User {
 			@AttributeOverride(name = "email", column = @Column(nullable = false)),
 			@AttributeOverride(name = "phone", column = @Column(nullable = false)) })
 	private Profile profile;
+
+	@ManyToMany
+	@JoinTable(name = "following_table", joinColumns = { @JoinColumn(name = "follower_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "following_id") })
+	private List<User> following = new ArrayList<>();
+
+	@ManyToMany(mappedBy = "following", fetch = FetchType.EAGER)
+	private List<User> followers = new ArrayList<>();
+
 }
