cebbfd2175a2083214c434dafd06b52004b031c5
markjt031 <markjt031@gmail.com>
2023-11-30 11:57:13 -0500

    post user working

--- socialmedia/src/main/java/com/cooksys/socialmedia/repositories/UserRepository.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.cooksys.socialmedia.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,6 @@ import com.cooksys.socialmedia.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-	List<User> findAllByDeletedFalse();
+	Optional<List<User>> findAllByDeletedFalse();
+	Optional<User> findByCredentialsUsername(String username);
 }
--- socialmedia/src/main/java/com/cooksys/socialmedia/services/impl/UserServiceImpl.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/services/impl/UserServiceImpl.java
@@ -1,15 +1,20 @@
 package com.cooksys.socialmedia.services.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.cooksys.socialmedia.dtos.UserRequestDto;
 import com.cooksys.socialmedia.dtos.UserResponseDto;
 import com.cooksys.socialmedia.entities.User;
+import com.cooksys.socialmedia.exceptions.BadRequestException;
 import com.cooksys.socialmedia.mappers.UserMapper;
 import com.cooksys.socialmedia.repositories.UserRepository;
 import com.cooksys.socialmedia.services.UserService;
 import lombok.RequiredArgsConstructor;
+
+
 import org.springframework.stereotype.Service;
 
 import lombok.NoArgsConstructor;
@@ -26,8 +31,27 @@ public class UserServiceImpl implements UserService{
 	
 	@Override
 	public List<UserResponseDto> getAllUsers() {
-		return userMapper.entitiesToDtos(userRepository.findAllByDeletedFalse());
+		return userMapper.entitiesToDtos(userRepository.findAllByDeletedFalse().get());
 		 
-		
+	}
+
+
+	@Override
+	public UserResponseDto createUser(UserRequestDto userRequestDto) {
+		if (userRequestDto == null || userRequestDto.getCredentials()==null || userRequestDto.getCredentials().getUsername()==null
+				|| userRequestDto.getCredentials().getPassword()==null || userRequestDto.getProfile()== null || userRequestDto.getProfile().getEmail()==null) {
+			throw new BadRequestException("include all required fields");
+		}
+		User userToCreate = userMapper.requestDtoToEntity(userRequestDto);
+		Optional<User> optionalUser = userRepository.findByCredentialsUsername(userToCreate.getCredentials().getUsername());
+		if (optionalUser.isPresent()) {
+			User foundUser=optionalUser.get();
+			if (foundUser.isDeleted()==true) {
+				foundUser.setDeleted(false);
+				return userMapper.entityToDto(userRepository.saveAndFlush(foundUser));
+			}
+			else throw new BadRequestException("user already exists");
+		}
+		return userMapper.entityToDto(userRepository.saveAndFlush(userToCreate));
 	}
 }
