f65465f3b9ca8a66d538eac33dd3cea58368d133
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-11-30 09:29:44 -0600

    Update ValidateServiceImpl.java

--- socialmedia/src/main/java/com/cooksys/socialmedia/services/impl/ValidateServiceImpl.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/services/impl/ValidateServiceImpl.java
@@ -1,6 +1,10 @@
 package com.cooksys.socialmedia.services.impl;
 
 import com.cooksys.socialmedia.services.ValidateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class ValidateServiceImpl implements ValidateService{
 }
