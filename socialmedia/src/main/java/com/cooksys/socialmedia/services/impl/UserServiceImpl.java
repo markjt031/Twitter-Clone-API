11a9b729b4aa4506b04ab2f7f4190b9c30719a4d
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-11-30 09:29:34 -0600

    Update UserServiceImpl.java

--- socialmedia/src/main/java/com/cooksys/socialmedia/services/impl/UserServiceImpl.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/services/impl/UserServiceImpl.java
@@ -1,6 +1,10 @@
 package com.cooksys.socialmedia.services.impl;
 
 import com.cooksys.socialmedia.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 }
