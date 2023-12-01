a50d633d02cdd7ba2bdf7e6c020a15a2d652a19e
PhamJimmy <JimmyPhamRes@gmail.com>
2023-12-01 08:24:19 -0800

    implemented GET validate/username/exists/@{username} and GET validate/username/available/@{username}

--- socialmedia/src/main/java/com/cooksys/socialmedia/repositories/UserRepository.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/repositories/UserRepository.java
@@ -1,17 +1,18 @@
 package com.cooksys.socialmedia.repositories;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.cooksys.socialmedia.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.cooksys.socialmedia.entities.Tweet;
-import com.cooksys.socialmedia.entities.User;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<List<User>> findAllByDeletedFalse();
-	Optional<User> findByCredentialsUsername(String username);
-	
+  Optional<List<User>> findAllByDeletedFalse();
+
+  Optional<User> findByCredentialsUsername(String username);
+
+  Optional<User> findByCredentialsUsernameIgnoreCase(String username);
+
 }
--- socialmedia/src/main/java/com/cooksys/socialmedia/services/impl/ValidateServiceImpl.java
+++ socialmedia/src/main/java/com/cooksys/socialmedia/services/impl/ValidateServiceImpl.java
@@ -1,10 +1,23 @@
 package com.cooksys.socialmedia.services.impl;
 
+import com.cooksys.socialmedia.repositories.UserRepository;
 import com.cooksys.socialmedia.services.ValidateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ValidateServiceImpl implements ValidateService{
+public class ValidateServiceImpl implements ValidateService {
+
+  private final UserRepository userRepository;
+
+  @Override
+  public boolean usernameExists(String username) {
+    return userRepository.findByCredentialsUsernameIgnoreCase(username).isPresent();
+  }
+
+  @Override
+  public boolean usernameAvailable(String username) {
+    return !usernameExists(username);
+  }
 }
